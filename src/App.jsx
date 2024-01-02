dae7c25d422d8fd4fa370a3ff49d1ff78d18a89f
Shubham Sanwariya <shubhamsanvariyait@gmail.com>
2024-01-02 13:42:37 +0530

    adding BrowserRouter and routes for home, edit & existingRooms

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom"
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import "../node_modules/bootstrap/dist/js/bootstrap.min.js"
-import AddRoom from "./components/room/AddRoom.jsx"
 import ExistingRooms from "./components/room/ExistingRooms.jsx"
+import Home from "./components/home/Home.jsx"
+import EditRoom from "./components/room/EditRoom.jsx"
 
 function App() {
 
   return (
     <>
-    <AddRoom />
-     <ExistingRooms />
+    <main>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+            <Route path="/edit-room/:roomId" element={<EditRoom />}/>
+          <Route path="/existing-rooms" element={<ExistingRooms />}/>
+        </Routes>
+      </Router>
+    </main>
     </>
   )
 }
