1a55b1d38a1fc449ce650c20259fe2782eca95c3
Shubham Sanwariya <shubhamsanvariyait@gmail.com>
2024-01-02 23:35:21 +0530

    adding Routing for AddRoom

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,22 @@ import "../node_modules/bootstrap/dist/js/bootstrap.min.js"
 import ExistingRooms from "./components/room/ExistingRooms.jsx"
 import Home from "./components/home/Home.jsx"
 import EditRoom from "./components/room/EditRoom.jsx"
+import AddRoom from "./components/room/AddRoom.jsx"
 
 function App() {
 
   return (
     <>
-    <main>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />}/>
-            <Route path="/edit-room/:roomId" element={<EditRoom />}/>
-          <Route path="/existing-rooms" element={<ExistingRooms />}/>
-        </Routes>
-      </Router>
-    </main>
+      <main>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/edit-room/:roomId" element={<EditRoom />} />
+            <Route path="/existing-rooms" element={<ExistingRooms />} />
+            <Route path="/add-room" element={<AddRoom />} />
+          </Routes>
+        </Router>
+      </main>
     </>
   )
 }
