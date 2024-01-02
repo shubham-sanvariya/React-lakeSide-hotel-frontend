9fe067767175d03157b03dd6b934735740d5a1bd
Shubham Sanwariya <shubhamsanvariyait@gmail.com>
2024-01-02 13:39:04 +0530

    adding apiFunction for deleting roomById

--- src/components/utils/ApiFunctions.js
+++ src/components/utils/ApiFunctions.js
@@ -55,3 +55,13 @@ export async function getAllRooms(){
         throw new Error("Error fetching rooms");
     }
 }
+
+// This function deletes a room by the Id
+export async function deleteRoom(roomId){
+    try {
+        const result = await api.delete(`/rooms/delete/room/${roomId}`);
+        return result.data;
+    } catch (error) {
+        throw new Error('Error deleting room');
+    }
+}
