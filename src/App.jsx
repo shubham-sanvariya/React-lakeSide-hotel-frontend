595892e5ee9a9fab323bf81222be6238d7a765fc
Shubham Sanwariya <shubhamsanvariyait@gmail.com>
2024-01-07 18:50:58 +0530

    adding existing bookings

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import RoomListing from "./components/room/RoomListing.jsx"
 import Admin from "./components/admin/Admin.jsx"
 import Checkout from "./components/bookings/Checkout.jsx"
 import BookingSuccess from "./components/bookings/BookingSuccess.jsx"
+import Bookings from "./components/bookings/Bookings.jsx"
 
 function App() {
 
@@ -28,6 +29,7 @@ function App() {
             <Route path="/browse-all-rooms" element={<RoomListing />} />
             <Route path="/admin" element={<Admin />} />
             <Route path="/booking-success" element={<BookingSuccess />} />
+            <Route path="/existing-bookings" element={<Bookings />} />
           </Routes>
         </Router>
         <Footer/>
