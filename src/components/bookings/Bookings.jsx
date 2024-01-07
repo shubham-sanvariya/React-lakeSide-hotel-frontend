df254dadb758e689ceaf0e8a51c24fbc16caeb4a
Shubham Sanwariya <shubhamsanvariyait@gmail.com>
2024-01-07 18:50:10 +0530

    adding Bookings Component

---
+++ src/components/bookings/Bookings.jsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from 'react';
+import {cancelBooking, getAllBookings} from '../utils/ApiFunctions';
+import Header from '../Common/Header';
+import BookingTable from './BookingTable';
+
+const Bookings = () => {
+
+    const[bookingInfo, setBookingInfo] = useState([]);
+    const[isLoading, setIsLoading] = useState(false);
+    const[error, setError] = useState("");
+
+    useEffect(() => {
+        setTimeout(() => {
+           getAllBookings().then((data) => {
+            console.log(data)
+            setBookingInfo(data);
+            setIsLoading(false);
+           }).catch((error) => {
+            setError(error.message);
+            setIsLoading(false);
+           })
+        }, 1000);
+    }, []);
+
+    const handleBookingCancellation = async(bookingId) => {
+        try {
+            await cancelBooking(bookingId);
+            const data = await getAllBookings();
+            setBookingInfo(data)
+        } catch (error) {
+            setError(error.message);
+        }
+    }
+
+    return (
+        <section className='container' style={{backgroundColor : "whitesmoke"}}>
+            <Header title={"Existing Bookings"}/>
+            {error && (<div className='text-danger'>{error}</div>)}
+            {isLoading ? (<div>Loading existing bookings</div>)
+            :(
+                <BookingTable
+                 bookingInfo={bookingInfo}
+                 handleBookingCancellation={handleBookingCancellation}
+                />
+            )}
+        </section>
+    );
+}
+
+export default Bookings;
