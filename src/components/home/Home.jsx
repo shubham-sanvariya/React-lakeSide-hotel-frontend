3ea0c24bf9994c146f25f639f49661ce5b075b5b
Shubham Sanwariya <shubhamsanvariyait@gmail.com>
2024-01-11 15:08:15 +0530

    adding room serach  component to the home page

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -3,12 +3,14 @@ import MainHeader from '../layout/MainHeader'
 import Parallax from '../Common/Parallax';
 import HotelServices from '../Common/HotelServices';
 import RoomCarousel from '../Common/RoomCarousel';
+import RoomSearch from '../Common/RoomSearch';
 
 const Home = () => {
   return (
     <section>
       <MainHeader />
       <section className='container'>
+        <RoomSearch/>
         <RoomCarousel />
         <Parallax />
         <RoomCarousel />
