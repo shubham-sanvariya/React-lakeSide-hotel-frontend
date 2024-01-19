6d39b059a4f9b8db08329191331e1a1d4126c752
Shubham Sanwariya <shubhamsanvariyait@gmail.com>
2024-01-19 16:00:55 +0530

    routing Require auth for all-bookings

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ import Registration from "./components/auth/Registration.jsx"
 import Profile from "./components/auth/Profile.jsx"
 import Logout from "./components/auth/Logout.jsx"
 import AuthProvider from "./components/auth/AuthProvider.jsx"
+import RequireAuth from "./components/auth/RequireAuth.jsx"
 
 function App() {
 
@@ -32,7 +33,14 @@ function App() {
             <Route path="/edit-room/:roomId" element={<EditRoom />} />
             <Route path="/existing-rooms" element={<ExistingRooms />} />
             <Route path="/add-room" element={<AddRoom />} />
-            <Route path="/book-room/:roomId" element={<Checkout />} />
+              <Route
+                path="/book-room/:roomId"
+                element={
+                  <RequireAuth>
+                    <Checkout />
+                  </RequireAuth>
+                }
+              />
             <Route path="/browse-all-rooms" element={<RoomListing />} />
             <Route path="/admin" element={<Admin />} />
             <Route path="/booking-success" element={<BookingSuccess />} />
