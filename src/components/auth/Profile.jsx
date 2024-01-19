1ff095a7a7768dbdc1145ff1155aa22788c9ec1f
Shubham Sanwariya <shubhamsanvariyait@gmail.com>
2024-01-19 11:27:59 +0530

    importing getBooking ByUserId

--- src/components/auth/Profile.jsx
+++ src/components/auth/Profile.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
 import moment from "moment"
-import { deleteUser, getUser } from "../utils/ApiFunctions"
+import { deleteUser, getBookingsByUserId, getUser } from "../utils/ApiFunctions"
 
 const Profile = () => {
     const [user, setUser] = useState({
